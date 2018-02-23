5cc7c1b3984a298aaa40b6553bc15e35a04092e1
tmcjunkinmarquis <tmcjunkinmarquis@gmail.com>
2018-02-23 16:19:26 -0700

    Include first test and scss functioning

--- test/App-test.js
+++
@@ -1,5 +0,0 @@
-import { expect } from 'chai';
-import App from '../lib/App'
-
-describe('Example Test File', () => {
-})
---
+++ test/App.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {mount, shallow} from 'enzyme';
+import App from '../lib/App';
+
+describe('Example Test File', () => {
+  it('should fucking work', () => {
+    expect(true).toEqual(true);
+  })
+})
