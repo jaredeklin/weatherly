34cc80312aa64ea688af2c5edf817706f1ea3798
Jared Eklin <jaredeklin@gmail.com>
2018-02-25 11:07:11 -0700

    Fix merge issue

--- test/App.test.js
+++ test/App.test.js
@@ -1,9 +1,42 @@
 import React from 'react';
-import {mount, shallow} from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import App from '../lib/App';
 
-describe('Example Test File', () => {
-  it('should fucking work', () => {
-    expect(true).toEqual(true);
-  })
-})
+describe('App', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    localStorage.clear();
+    wrapper = shallow(<App />);
+  });
+
+  it('should exist', () => {
+    
+    expect(wrapper).toBeDefined();
+  });
+
+  it('Should render the Search and CurrentWeather components', () => {
+    localStorage.setItem('location', 'Denver, CO')
+    wrapper = mount(<App />)
+    expect(wrapper.find('Search').length).toEqual(1);
+  });
+
+  it('Should render the Welcome component', () => {
+    expect(wrapper.find('Welcome').length).toEqual(1);
+  });
+
+  it('Should initially have set states of a toggle, true; an undefined city, and error of false', () => {
+    
+    expect(wrapper.state()).toEqual(
+      {
+        tenDayToggle: true, 
+        city: undefined, 
+        errorPresent: false
+      });
+  });
+
+  // it('should set the state of our location', () => {
+  //   wrapper.instance().setLocation('denver')
+  //   expect(wrapper.state('location')).toEqual( 'denver' );
+  // });
+});
